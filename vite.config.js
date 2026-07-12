41d01350a6f47b7658dc8a59081e2f998f001da2
yyymml <yml9869@qq.com>
2026-07-12 10:16:33 +0800

    Set base path for Aone-web project

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 下面这一行是新增的，仓库名严格匹配你的项目名 Aone-web
   base: '/Aone-web/',
-  plugins: [vue()],
+  plugins: [vue()]
 })

86ce3181ff2f9a5715196d1d58d85db4402a5ca9
yyymml <yml9869@qq.com>
2026-07-12 10:20:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Aone-web/',
-  plugins: [vue()]
+  plugins: [react()]
 })

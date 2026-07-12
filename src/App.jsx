d9f3dad8c89315e763b74d6d53a48292d9390516
yyymml <yml9869@qq.com>
2026-07-12 10:47:59 +0800

    Wrap routes in HashRouter for routing support

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -14,20 +14,22 @@ import './App.css'
 export default function App() {
   return (
     <div className="app">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/paper-craft" element={<PaperCraft />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/shroud" element={<ShroudTryOn />} />
-          <Route path="/pet-services" element={<PetServices />} />
-          <Route path="/digital-life" element={<DigitalLife />} />
-          <Route path="/memorial" element={<Memorial />} />
-          <Route path="/community" element={<Community />} />
-        </Routes>
-      </main>
-      <Footer />
+      <HashRouter>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/paper-craft" element={<PaperCraft />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/shroud" element={<ShroudTryOn />} />
+            <Route path="/pet-services" element={<PetServices />} />
+            <Route path="/digital-life" element={<DigitalLife />} />
+            <Route path="/memorial" element={<Memorial />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </main>
+        <Footer />
+      </HashRouter>
     </div>
   )
 }

ea8f82a2583f328ceb4b4ab5ed6eb9442389c365
yyymml <yml9869@qq.com>
2026-07-12 15:21:21 +0800

    fix: 自动注入CSP，清理html冗余代码，修复eval拦截页面空白

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Aone-web/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-csp',
+      transformIndexHtml(html) {
+        return html.replace(
+          '<head>',
+          `<head>
+<meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;">`
+        )
+      }
+    }
+  ],
   build: {
     terserOptions: {
       compress: {

6226fdcff5d5d4880d75353f9ff482de96a59f6f
yyymml <yml9869@qq.com>
2026-07-12 10:07:35 +0800

    Replace React plugin with Vue and update base path
    fix: 适配github pages子路径，修复白屏

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  base: '/anwang-web/'
+  // 下面这一行是新增的，仓库名严格匹配你的项目名 Aone-web
+  base: '/Aone-web/',
+  plugins: [vue()],
 })

1ee3c2ce4827d48967d7a3599b6530b1ff7bd6a9
yyymml <yml9869@qq.com>
2026-07-12 14:45:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Aone-web/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    terserOptions: {
+      compress: {
+        drop_eval: true // 移除所有eval代码，规避CSP拦截
+      }
+    }
+  }
 })
